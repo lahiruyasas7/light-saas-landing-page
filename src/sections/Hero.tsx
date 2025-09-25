f4c83def6e325807eb65b77ab4c9d121861f3241
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-09-25 18:45:57 +0530

    removed md:mb-12 from section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -7,7 +7,7 @@ import NoodleImage from "@/assets/noodle.png";
 
 export const Hero = () => {
   return (
-    <section className="pt-8 pb-20 md:mb-12 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left_,#183EC2,#EAEEFE_100%)] overflow-x-clip">
+    <section className="pt-8 pb-20 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left_,#183EC2,#EAEEFE_100%)] overflow-x-clip">
       <div className="container">
         <div className="md:flex items-center">
           <div className="md:w-[478px]">
