74a8aa58a00077eca602f162eefd1d37a210b4be
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-09-27 23:59:05 +0530

    wip: created upper section

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,3 +1,18 @@
 export const ProductShowcase = () => {
-  return null;
+  return (
+    <div className="section py-24 px-5">
+      <div className="flex flex-col text-center justify-center items-center gap-4">
+        <h4 className="text-sm inline-flex border border-[#222222/10] px-3 py-1 rounded-lg">
+          Boost your productivity
+        </h4>
+        <h2 className="font-bold text-[32px] tracking-[-5%] leading-[32px] bg-gradient-to-b from-black to-blue-800 text-transparent bg-clip-text">
+          A more effective way to track progress
+        </h2>
+        <p className="font-normal text-[22px] text-[#010D3E] tracking-[-2.5%] leading-[30px]">
+          Celebrate the joy of accomplishment with an app designed to track your
+          progress and motivate your efforts.
+        </p>
+      </div>
+    </div>
+  );
 };
