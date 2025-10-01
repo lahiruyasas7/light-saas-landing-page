c788627e09d47beb969d2a00680505e35c54a77d
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-10-01 14:51:57 +0530

    style: added text-color changeable

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -4,6 +4,7 @@ import clsx from "clsx";
 type CustomButtonProps = {
   children: React.ReactNode;
   bgColor?: string;   // Tailwind color class, e.g. "bg-blue-500"
+  textColor?: string; // Tailwind text color class, e.g. "text-white"
   width?: string;     // Tailwind width class, e.g. "w-32" or custom style
   height?: string;    // Tailwind height class, e.g. "h-12"
   className?: string; // Extra classes (e.g. rounded-lg, shadow-md)
@@ -13,6 +14,7 @@ type CustomButtonProps = {
 function CustomButton({
   children,
   bgColor = "bg-black",
+  textColor = "text-white",
   width = "w-[115px]",
   height = "h-[40px]",
   className,
@@ -22,8 +24,9 @@ function CustomButton({
     <button
       onClick={onClick}
       className={clsx(
-        "text-white flex items-center justify-center rounded-md",
+        "flex items-center justify-center rounded-md",
         bgColor,
+        textColor,
         width,
         height,
         className
